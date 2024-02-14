8c3db28385d22e4c64e0ff215e62137b7bae5362
Arnemix <contact.arnemix@gmail.com>
2024-02-14 20:02:02 +0100

    Finalisation du CV beta

--- src/components/cv/CV.jsx
+++ src/components/cv/CV.jsx
@@ -7,12 +7,14 @@ import { MdEmail } from "react-icons/md";
 import { FaBirthdayCake } from "react-icons/fa";
 import { getAge, getIcon } from "../../utils/utils";
 import { IoCalendarClear } from "react-icons/io5";
-
+import CVPicture from "../../assets/cv-picture.png";
 function CV(props) {
     return (
         <div className="cv-container">
             <div className="cv-sidebar">
-                <div className="cv-picture">{/* Mettre la photo */}</div>
+                <div className="cv-picture">
+                    <img src={CVPicture} alt="profil" />
+                </div>
                 <div className="cv-coordinates">
                     <h2>COORDONNÉES</h2>
                     <p>
@@ -47,10 +49,83 @@ function CV(props) {
                             );
                         })}
                     </div>
+                    <div className="cv-language">
+                        <h2>Langues</h2>
+                        <div className="cv-language-container">
+                            {data["cv-language"].map((language) => {
+                                return (
+                                    <div key={language["id"]} className="cv-language-item">
+                                        <p>{language["name"]}</p>
+                                        <div className="progress-container">
+                                            <div className="progress" style={{ width: `${language["level"]}%` }}></div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="cv-hobbies">
+                        <h2>Hobbies</h2>
+                        <div className="cv-hobbies-container">
+                            {data["cv-hobbies"].map((hobby) => {
+                                return (
+                                    <div key={hobby["id"]} className="cv-hobby-item">
+                                        <p>{hobby["name"]}</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="cv-main">
-                <h2>Main</h2>
+                <div className="cv-description">
+                    <h2>Description</h2>
+                    <p>{data["cv-personnal-info"]["description"]}</p>
+                </div>
+                <div className="cv-education">
+                    <h2>Parcours scolaire</h2>
+                    {data["cv-education"].map((education) => {
+                        return (
+                            <div key={education["id"]} className="cv-education-item">
+                                <p>
+                                    <b>{education["certification"]}</b> : {education["title"]}
+                                </p>
+                                <p>
+                                    <b>{education["schoolName"]}</b> - Bruxelles
+                                </p>
+                                <hr />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="cv-experience">
+                    <h2>Parcours professionnel</h2>
+                    {data["cv-experience"].map((experience) => {
+                        return (
+                            <div key={experience["id"]} className="cv-experience-item">
+                                <p>
+                                    <b>{experience["title"]}</b> - {experience["jobType"]} - {experience["duration"]}
+                                </p>
+                                <p>
+                                    <span>{experience["company"]} - </span> {experience["location"]}
+                                </p>
+                                <ul>
+                                    <li>{experience["description"]}</li>
+                                </ul>
+                                <hr />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="cv-certifications">
+                    <h2>Certifications</h2>
+                    <ul>
+                        {data["cv-certifications"].map((certification) => {
+                            return <li key={certification["id"]}>{certification["name"]}</li>;
+                        })}
+                    </ul>
+                </div>
             </div>
         </div>
     );
